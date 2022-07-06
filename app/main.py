82a7061af006ebcb5d34161bb829ff19a306ca71
Aaron Dominguez <aronpes94@gmail.com>
2022-07-06 22:14:54 +0200

    Return Random card and add template folder

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
-from repository import CardRepo
+import random
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from models import Card, Response
+from repository import CardRepo
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/card/")
@@ -19,10 +28,10 @@ async def create(card: Card):
     return Response(code=200, status="Ok", message="Success save data").dict(exclude_none=True)
 
 
-@app.get("/card/{id}")
-async def get_id(id: str):
+@app.get("/card/{id}", response_class=HTMLResponse)
+async def get_id(request: Request, id: str):
     _card = await CardRepo.retrieve_id(id)
-    return Response(code=200, status="Ok", message="Success retrieve data", result=_card).dict(exclude_none=True)
+    return templates.TemplateResponse("item.html", {"request": request, "id": id, "card": _card})
 
 
 @app.post("/card/update")
@@ -35,3 +44,10 @@ async def update(card: Card):
 async def delete(id: str):
     await CardRepo.delete(id)
     return Response(code=200, status="Ok", message="Success delete data").dict(exclude_none=True)
+
+
+@app.get("/card/random/", response_class=HTMLResponse)
+async def random_card(request: Request):
+    _cardList = await CardRepo.retrieve()
+    _card = random.choice(_cardList)
+    return templates.TemplateResponse("item.html", {"request": request, "id": id, "card": _card})
